b751306b8664ebe45cf1bc915323187cd4b78afb
luizfelipe798 <luizvignoto@gmail.com>
2025-09-03 17:15:33 -0300

    atualização da porta para conexão com o banco de dados.

--- core/conexao_mysql.php
+++ core/conexao_mysql.php
@@ -3,7 +3,7 @@
     {
         $servidor = 'localhost';
         $banco = 'blog';
-        $port = 3306;
+        $port = 3307;
         $usuario = "root";
         $senha = '815674815';
         $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $port);
